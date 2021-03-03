49a326f5e49380cf5280e3c187329f3c5f9f0f5d
nana_s <nana_a@waku-2.com>
2021-03-03 12:40:26 +0900

    section4

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,44 @@
+// const userName = 'Max';
+// let age = 30;
 
-const button = document.querySelector('button');
+// age = 29;
 
-function clickHandler(message:string) {
-  console.log('Clicked!' + message);
+// function add(a:number, b: number) {
+//   let result;
+//   result = a - b;
+//   return result;
+// }
+
+
+// if (age >= 20) {
+//   let isAdult = true;
+// }
+ 
+// console.log(isAdult);　// エラーになる
+
+// let const はブロックスコープを採用している
+
+
+// ひとつの式と戻り値のみを返す場合は{}とreturnを省略することができる
+// 右側の引数にはデフォルト値を設定できるaだけにデフォルト値を渡すことはできない（パラメータをスキップすることができない）
+const add = (a: number , b: number = 1) => a - b;
+
+// 引数がひとつのときは（）を省略することができる
+const printOutput : (output: string | number) => void = output => {
+  console.log(output);
 }
 
-if(button) {
-  button.addEventListener('click',clickHandler.bind(null, "You're welcome!"));
-}
+printOutput(add(2));
+
+
+const hobbies= ['Sports', 'Cooking'];
+const activeHobbies = ['Hiking'];
+// スプレット演算子（...）配列やオブジェクトの値を取り出して、展開する
+// コピーして別物として追加される
+activeHobbies.push(...hobbies);
+
+const hobby = hobbies[0];
+
+
+
+
