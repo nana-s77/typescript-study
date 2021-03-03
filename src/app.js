7a0f7aa1f1cdcd7d2bab872e24d8e220ab847bd3
nana_s <nana_a@waku-2.com>
2021-03-03 10:26:29 +0900

    section3 tsconfig.json設定とデバック

--- src/app.js
+++ src/app.js
@@ -1,9 +1,7 @@
-"use strict";
-const button = document.querySelector('button');
+var button = document.querySelector('button');
 function clickHandler(message) {
     console.log('Clicked!' + message);
 }
 if (button) {
     button.addEventListener('click', clickHandler.bind(null, "You're welcome!"));
 }
-//# sourceMappingURL=app.js.map
